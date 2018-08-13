1cdcf270ad6859e978ae0cd8e5f4f8eb40f861b1
amirmazinani <amir_mz75@hotmail.com>
2018-08-13 18:25:49 +0430

    create header & footer & top of page

--- public_html/index.php
+++ public_html/index.php
@@ -9,8 +9,53 @@
     <link rel="stylesheet" type="text/css" href="//cdn.lemoni.<?= $tld ?>/styles/fonts/1.11.14.css" />
     <link rel="stylesheet" type="text/css" href="//cdn.lemoni.<?= $tld ?>/styles/grid/2.4.12.css" />
     <link rel="stylesheet" type="text/css" href="//cdn.lemoni.<?= $tld ?>/styles/lemoni/2.21.0.min.css" />
+    <link rel="stylesheet" type="text/css" href="/styles/css/main.css" />
 </head>
-<body>
+<body class="">
 
+<header class="-t-al-l c-main -pos-f -top-0 -left-0 -w-11 -bx-s-b">
+    <a href="#" class="transition -fnt-s-h4 c-main">
+        <i class="-fnt-icon-th-1"></i>
+    </a>
+</header>
+
+<section id="top" class="-bx-s-b -p-3 -h-100">
+    <div class="-b-c-eee h-100d dis-f flex-d-c jus-c-sb">
+        <div class="dis-f jus-c-sb h-100d">
+            <div class="-w-12 dis-f flex-d-c jus-c-sa">
+                <div class="">
+                    <h1 class="fnt-montserrat-b -pb-3">Artist Painter</h1>
+                    <h2 class="">Shadi Rahiminik</h2>
+                </div>
+                <div>
+                    <a class="-c-white" href="#">Go To my painting</a>
+                </div>
+            </div>
+            <div class="-w-12 -t-al-c dis-f al-it-c">
+                <img class="top-img" src="/uploads/images/1.jpg" alt="">
+            </div>
+        </div>
+        <footer class="-t-al-r c-main -pos-f -bottom-0 -left-0 -w-11 -bx-s-b">
+            <a href="#" class="transition -fnt-s-h4 -pos-r">
+                <i class="-fnt-icon-twitter"></i>
+            </a>
+            <a href="#" class="transition -fnt-s-h4 -p-1">
+                <i class="-fnt-icon-facebook-3"></i>
+            </a>
+            <a href="#" class="transition -fnt-s-h4 -p-1">
+                <i class="-fnt-icon-instagram"></i>
+            </a>
+            <a href="#" class="transition -fnt-s-h4 -p-1">
+                <i class="-fnt-icon-telegram"></i>
+            </a>
+        </footer>
+    </div>
+</section>
+
+
+
+
+<script src="//cdn.lemoni.<?= $tld ?>/scripts/jquery/3.2.1.min.js"></script>
+<script src="/scripts/main.js"></script>
 </body>
 </html>
---
+++ public_html/scripts/main.js
@@ -0,0 +1,5 @@
+$(function () {
+    function chng_top_img() {
+        console.log('ok');
+    }
+});
