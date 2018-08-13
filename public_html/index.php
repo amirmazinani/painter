f3c4cfdc68f73f3825d488b9e2ea3840abe9f99f
amirmazinani <amir_mz75@hotmail.com>
2018-08-13 12:57:19 +0430

    change format .html to .php

---
+++ public_html/index.php
@@ -0,0 +1,16 @@
+<?php $tld = PHP_OS !== 'WINNT' ? 'ir' : 'w3r' ?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>shadi rahiminic</title>
+    <link rel="stylesheet" type="text/css" href="//cdn.lemoni.<?= $tld ?>/styles/fonts/1.11.14.css" />
+    <link rel="stylesheet" type="text/css" href="//cdn.lemoni.<?= $tld ?>/styles/grid/2.4.12.css" />
+    <link rel="stylesheet" type="text/css" href="//cdn.lemoni.<?= $tld ?>/styles/lemoni/2.21.0.min.css" />
+</head>
+<body>
+
+</body>
+</html>
