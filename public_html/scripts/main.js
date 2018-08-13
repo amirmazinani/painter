4190090c2bf9e725264a063332125d061c46fb48
amirmazinani <amir_mz75@hotmail.com>
2018-08-14 01:17:22 +0430

    chng_top_img function

--- public_html/scripts/main.js
+++ public_html/scripts/main.js
@@ -1,5 +1,21 @@
 $(function () {
     function chng_top_img() {
-        console.log('ok');
+        if($('.top-img').length > 0){
+
+            $(".top-img:gt(0)").hide();
+
+            setInterval(function() {
+                $('.top-img:first')
+                    .fadeOut(1000)
+                    .next()
+                    .fadeIn(1000)
+                    .end()
+                    .appendTo('.all-top-img');
+            },  7000);
+
+
+            console.log('ok');
+        }
     }
+    chng_top_img();
 });
