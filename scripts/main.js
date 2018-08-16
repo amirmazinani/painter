bb0f2febfe14681db435ad1d4d34942120b74411
amirmazinani <amir_mz75@hotmail.com>
2018-08-16 22:17:58 +0430

    complete portfolios

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 $(function () {
+    //fade slider in top
     function chng_top_img() {
         if($('.top-img').length > 0){
 
@@ -12,10 +13,52 @@ $(function () {
                     .end()
                     .appendTo('.all-top-img');
             },  7000);
-
-
-            console.log('ok');
         }
     }
     chng_top_img();
+
+    //repeat and create item in portfolios
+    function repeatItem() {
+        for(let i = 1 ; i <= 19 ; i++){
+            let item =
+                `<div class="item">
+                    <div class="-pos-r -cur-point -brd-r-3 -ovf-h">
+                        <p class="transition -pos-a -top-0 -right-0 -c-black -m-3 dis-f flex-d-rw al-it-c">
+                            <i class="-fnt-icon-plus transition -dis-ib -brd-r-c sqr -t-al-c -b-c-white"></i>
+                            <span class="-fnt-s-p transition -dis-ib -pr-1">click to zoom</span>
+                        </p>
+                        <img class="-w-11 -brd-r-3" src="/uploads/images/portfolios/${i}.jpg" alt="painting number ${i}">
+                    </div>
+                </div>`;
+
+            $('#portfolios > main').append(item);
+        }
+    }
+    repeatItem();
+
+    //resize item by click
+    $('#portfolios > main > .item > div').on('click',function () {
+        if($(window).width() >= 490){
+            if($(this).closest('.item').hasClass('open')){
+                $(this).closest('.item').removeClass('open');
+                $(this).find('i').toggleClass('-fnt-icon-plus -fnt-icon-cancel');
+                let topOffset = $(this).closest('.item').offset().top;
+                $(window).scrollTop(topOffset);
+            }else{
+                $('.item').removeClass('open');
+                $('.item > div > i').removeClass('-fnt-icon-cancel');
+                $('.item > div > i').addClass('-fnt-icon-plus');
+                $(this).closest('.item').addClass('open');
+                $(this).find('i').toggleClass('-fnt-icon-plus -fnt-icon-cancel');
+                let topOffset = $(this).closest('.item').offset().top;
+                $(window).scrollTop(topOffset);
+            }
+        }else{
+            $('body').toggleClass('-ovf-h');
+            $(this).closest('.item').toggleClass('big_open');
+            $(this).find('i').toggleClass('-fnt-icon-plus -fnt-icon-cancel');
+            let topOffset = $(this).closest('.item').offset().top;
+            $(window).scrollTop(topOffset);
+        }
+    });
 });
